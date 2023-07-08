9f03429e28b9177090c9cd8a0ba19212b174b511
donlHenard <pastor.git101@gmail.com>
2023-07-08 17:46:53 +0800

    Add articles and event for displaying

---
+++ script.js
@@ -0,0 +1,15 @@
+const articles = document.querySelectorAll('article');
+const navList = document.querySelectorAll('nav ul li');
+
+navList.forEach(list => {
+    list.addEventListener('click', () => {
+        navList.forEach(nav => {
+            nav.style.opacity = "1";
+        });
+        list.style.opacity = "0.5";
+    });
+});
+
+navList[0].addEventListener('click', () => {
+    articles[0].className = "show";
+});
